18c91cb1221dd0338465f94a7f74c32dea94e245
anntnzrb <anntnzrb@protonmail.com>
2021-11-08 09:55:07 -0500

    Mejora en métodos infix -> postfix

--- misc/src/main/java/club/annt/stack/Notacion.java
+++ misc/src/main/java/club/annt/stack/Notacion.java
@@ -3,6 +3,58 @@ package club.annt.stack;
 import java.util.Stack;
 
 public class Notacion {
+    /**
+     * Convierte la expresión dada de notación in-fija a post-fija.
+     * NOTA: Ésta versión no acepta paréntesis.
+     *
+     * @param expr Expresión en notación in-fija
+     * @return la expresión transformada a notación post-fija
+     */
+    public static String infixAPostfix(final String expr) {
+        final StringBuilder strBuff = new StringBuilder();
+        final Stack<String> strStack = new Stack<>();
+
+        /* separar expresión por elemento (sea operando u operador) e iterar */
+        final String[] exprElems = expr.split(" ");
+        for (final String elem : exprElems) {
+            /*
+             * sea el elemento un operador, mientras que el stack no esté
+             * vacío y la prioridad del operador sea menor o igual al último
+             * operador del stack, removerlo del stack y agregarlo a la
+             * expresión final.
+             */
+            if (isOperador(elem.charAt(0))) {
+                while (!strStack.isEmpty()
+                        && getPrioridad(elem.charAt(0))
+                        <= getPrioridad(strStack.peek().charAt(0))) {
+                    strBuff.append(strStack.pop()).append(" ");
+                }
+                /* si el operador es de mayor prioridad o el stack esté vació,
+                 * agregarlo al stack.
+                 */
+                strStack.push(elem);
+                /*
+                 * por otro lado, si no es un operador, es un operando,
+                 * simplemente agregarlo a la expresión final.
+                 */
+            } else {
+                strBuff.append(elem).append(" ");
+            }
+        }
+
+        /* vaciar stack, cuando la cantidad de elementos es distinta de 1,
+         * agregar un espacio al lado derecho
+         */
+        while (!strStack.isEmpty()) {
+            if (strStack.size() != 1) {
+                strBuff.append(strStack.pop()).append(" ");
+            } else {
+                strBuff.append(strStack.pop());
+            }
+        }
+
+        return strBuff.toString();
+    }
 
     /**
      * Convierte la expresión dada de notación in-fija a post-fija.
@@ -10,54 +62,109 @@ public class Notacion {
      * @param expr Expresión en notación in-fija
      * @return la expresión transformada a notación post-fija
      */
-    public static String infixAPostfix(final String expr) {
-        final StringBuilder str = new StringBuilder();
-        final Stack<Character> charStack = new Stack<>();
+    public static String infixAPostfixParens(final String expr) {
+        final StringBuilder strBuff = new StringBuilder();
+        final Stack<Character> chStack = new Stack<>();
 
-        for (int idx = 0; idx < expr.length(); idx++) {
-            final char caracter = expr.charAt(idx);
+        for (int i = 0; i < expr.length(); ++i) {
+            final char ch = expr.charAt(i);
 
-            if (isOperador(caracter)) {
-                if (!charStack.isEmpty()) {
-                    if (getPrioridad(caracter)
-                            <= getPrioridad(charStack.peek())) {
-                        while (!charStack.isEmpty()
-                                && !(getPrioridad(caracter)
-                                > getPrioridad(charStack.peek()))) {
-                            str.append(charStack.pop());
-                        }
-                    }
+            /*
+             * sea el ch un operador, mientras que el stack no esté
+             * vacío y la prioridad del operador sea menor o igual al último
+             * operador del stack, removerlo del stack y agregarlo a la
+             * expresión final.
+             */
+            if (isOperador(ch)) {
+                while (!chStack.isEmpty() && getPrioridad(ch)
+                        <= getPrioridad(chStack.peek())) {
+
+                    strBuff.append(chStack.pop());
                 }
-                charStack.push(caracter);
+
+                /* si el operador es de mayor prioridad o el stack esté vació,
+                 * agregarlo al stack.
+                 */
+                chStack.push(ch);
+            }
+
+            /* sea ch un '(', agregarlo al stack */
+            else if (ch == '(') { chStack.push(ch); }
+
+            /*
+             * sea ch un '(', agregarlo al stack, remover elementos del stack
+             * hasta encontrar su par.
+             */
+            else if (ch == ')') {
+                while (!chStack.isEmpty() && chStack.peek() != '(') {
+                    strBuff.append(chStack.pop());
+                }
+                chStack.pop();
+
+                /* si no es ninguno de los anteriores, entonces es operando */
             } else {
-                str.append(caracter);
+                strBuff.append(ch);
             }
         }
-        while (!charStack.isEmpty()) {
-            str.append(charStack.pop());
+
+        /* vaciar stack con los elementos que quedan y verificar expresión */
+        while (!chStack.isEmpty()) {
+            /* si se encuentra un '(' es porque la expresión es inválida */
+            if (chStack.peek() == '(') { return "Expresión inválida."; }
+            strBuff.append(chStack.pop());
         }
 
-        return str.toString();
+        return strBuff.toString();
     }
 
+    /**
+     * Evalúa la expresión pasada, esta expresión debe estar en notación
+     * post-fija.
+     *
+     * @param expr Expresión en notación post-fija a evaluar
+     * @return resultado numérico al evaluar la expresión
+     */
     public static Number evalPostfixExpr(final String expr) {
         final Stack<Number> numStack = new Stack<>();
 
-        for (int idx = 0; idx < expr.length(); idx++) {
-            final char caracter = expr.charAt(idx);
-
-            if (isOperando(caracter)) {
-                /* agregar al stack si no es un operador */
-                numStack.push(Character.getNumericValue(caracter));
-            } else {
+        /* separar expresión por elemento (sea operando u operador) e iterar */
+        final String[] exprElems = expr.split(" ");
+        for (final String elem : exprElems) {
+            /* agregar al stack si no es un operador */
+            if (!isOperador(elem.charAt(0))) {
+                numStack.push(Integer.parseInt(elem));
                 /* evaluar los dos últimos operandos con el operador actual */
-                numStack.push(evalOp(caracter, numStack.pop(),
+            } else {
+                numStack.push(evalOp(elem.charAt(0), numStack.pop(),
                         numStack.pop()));
             }
         }
 
         /* peek() y pop() sirven igualmente pero se vacía para ayudar al GC */
         return numStack.pop();
+    }
+
+    private static boolean isOperador(final char c) {
+        return c == '^'
+                || c == '*'
+                || c == '/'
+                || c == '+'
+                || c == '-';
+    }
+
+    private static byte getPrioridad(final char str) {
+        switch (str) {
+            case '^':
+                return 3;
+            case '*':
+            case '/':
+                return 2;
+            case '+':
+            case '-':
+                return 1;
+            default:
+                return -1;
+        }
     }
 
     private static Number evalOp(final char op, final Number x,
@@ -73,29 +180,6 @@ public class Notacion {
                 return x.floatValue() + y.floatValue();
             case '-':
                 return x.floatValue() - y.floatValue();
-            default:
-                return 0;
-        }
-    }
-
-    private static boolean isOperando(final char c) {
-        return !isOperador(c);
-    }
-
-    private static boolean isOperador(final char c) {
-        return c == '*' || c == '/' || c == '+' || c == '-';
-    }
-
-    private static byte getPrioridad(final char c) {
-        switch (c) {
-            case '^':
-                return 3;
-            case '*':
-            case '/':
-                return 2;
-            case '+':
-            case '-':
-                return 1;
             default:
                 return 0;
         }
--- misc/src/test/java/club/annt/stack/NotacionTest.java
+++ misc/src/test/java/club/annt/stack/NotacionTest.java
@@ -3,21 +3,32 @@ package club.annt.stack;
 import org.junit.jupiter.api.Test;
 
 import static club.annt.stack.Notacion.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NotacionTest {
 
     @Test
     void testInfixAPostfix() {
-        assertEquals("AB+", infixAPostfix("A+B"));
-        assertEquals("AB*C*D*", infixAPostfix("A*B*C*D"));
-        assertEquals("AB*A/C+", infixAPostfix("A*B/A+C"));
-        assertEquals("ABC*D/+", infixAPostfix("A+B*C/D"));
+        assertEquals("A B +", infixAPostfix("A + B"));
+        assertEquals("21 13 16 * + 18 -", infixAPostfix("21 + 13 * 16 - 18"));
+        assertEquals("2 3 4 * +", infixAPostfix("2 + 3 * 4"));
+        assertEquals("A B * 5 +", infixAPostfix("A * B + 5"));
+        assertEquals("A B * C /", infixAPostfix("A * B / C"));
     }
 
     @Test
     void testEvalPostfixExpr() {
-        assertEquals(20, evalPostfixExpr("45*").intValue());
-        assertEquals(41, evalPostfixExpr("984*+").intValue());
+        assertEquals(20, evalPostfixExpr("4 5 *").intValue());
+        assertEquals(41, evalPostfixExpr("9 8 4 * +").intValue());
+        assertEquals(29, evalPostfixExpr("13 16 +").intValue());
+        assertEquals(1372, evalPostfixExpr("15 34 + 28 *").intValue());
+    }
+
+    @Test
+    void testInfixAPostfixParens() {
+        assertEquals("Expresión inválida.",
+                infixAPostfixParens("((A-(B+C))*D^(E+F)"));
+        assertEquals("ABC+-DEF+^*",
+                infixAPostfixParens("((A-(B+C))*D^(E+F))"));
     }
 }
